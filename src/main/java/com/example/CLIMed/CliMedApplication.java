bd482132fff1c1b5e5650c8c5825e521db659887
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-09-15 22:32:38 -0300

    first commit - ex.2

---
+++ src/main/java/com/example/CLIMed/CliMedApplication.java
@@ -0,0 +1,13 @@
+package com.example.CLIMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliMedApplication.class, args);
+	}
+
+}
