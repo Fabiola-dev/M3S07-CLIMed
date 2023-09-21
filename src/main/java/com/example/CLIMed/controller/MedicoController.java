7d1131e07b3b21c185a43f67478da3b826e537b6
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-09-21 20:20:10 -0300

    config log

--- src/main/java/com/example/CLIMed/controller/MedicoController.java
+++ src/main/java/com/example/CLIMed/controller/MedicoController.java
@@ -4,6 +4,8 @@ import com.example.CLIMed.dtos.MedicoDTO;
 import com.example.CLIMed.model.MedicoModel;
 import com.example.CLIMed.repository.MedicoRepository;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ public class MedicoController {
 
     @Autowired
     MedicoRepository medicoRepository;
+    private static Logger LOGGER = LoggerFactory.getLogger(MedicoController.class);
 
     @PostMapping("medico")
     public ResponseEntity<MedicoModel> saveMedico(
@@ -26,11 +29,13 @@ public class MedicoController {
         var medicoModel = new MedicoModel();
 
         BeanUtils.copyProperties(medicoDTO,medicoModel);
+        LOGGER.info("Instância médico cadastrada" + medicoDTO.nome());
         return ResponseEntity.status(HttpStatus.CREATED).body(medicoRepository.save(medicoModel));
     }
 
     @GetMapping("medicos")
     public ResponseEntity<List<MedicoModel>> getAllMedicos(){
+        LOGGER.info("Listagem médico concluída.");
         return ResponseEntity.status(HttpStatus.OK).body(medicoRepository.findAll());
     }
 
@@ -39,10 +44,12 @@ public class MedicoController {
         Optional<MedicoModel> medicoO = medicoRepository.findById(id);
 
         if (medicoO.isEmpty()){
+            LOGGER.info("Médico não foi identificado.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado");
         }
 
         Optional<MedicoDTO> medicoDTO = medicoO.map(MedicoDTO::new);
+        LOGGER.info("Consulta do médico concluída");
         return ResponseEntity.status(HttpStatus.OK).body(medicoDTO.get());
     }
 
@@ -53,10 +60,12 @@ public class MedicoController {
 
         Optional<MedicoModel> medicoO = medicoRepository.findById(id);
         if (medicoO.isEmpty()) {
+            LOGGER.info("ID [" + id + "] não encontrado na base de dados.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado");
         }
         var medicoModel = medicoO.get();
         BeanUtils.copyProperties(medicoDTO, medicoModel);
+        LOGGER.info("Usuário de ID [" + id + "] atualizado na base de dados.");
         return ResponseEntity.status(HttpStatus.OK).body(medicoRepository.save(medicoModel));
     }
 
@@ -64,10 +73,12 @@ public class MedicoController {
     public ResponseEntity<Object> excluirMedico(@PathVariable UUID id){
         Optional<MedicoModel> medicoO = medicoRepository.findById(id);
         if (medicoO.isEmpty()){
+            LOGGER.info("ID [" + id + "] não identificado na base de dados.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Medico não encontrado");
         }
 
         medicoRepository.deleteById(id);
+        LOGGER.info("Usuário de ID [" + id + "] excluído da base de dados.");
         return ResponseEntity.status(HttpStatus.OK).body("Médico excluído com sucesso.");
     }
 }
